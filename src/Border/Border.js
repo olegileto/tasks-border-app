bc5260537c3f8aa1f2a6097212fe9fb301118af2
Олег <oleg@MacBook-Pro-Aloha.local>
2019-02-18 17:50:27 +0200

    Initial commit

---
+++ src/Border/Border.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './Border.css';
+
+class Border extends Component {
+  render() {
+    return (
+      <div className="Border">
+      </div>
+    );
+  }
+}
+
+export default Border;
